d71a7e70783e5be4bc807f5139ed1906a2a56ce1
Jimmy <jimmyrss1102@gmail.com>
2023-10-08 05:41:03 +0800

    fix: import `useLayoutEffect`
    chore: add config `cherry.config.ts`

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,18 @@
 import './App.css'
 import APIs from '@/apis/baseApi';
 import Cherry from 'cherry-markdown';
-import { useRef, useEffect } from 'react';
+import { useRef, useLayoutEffect } from 'react';
 import EngineApi from './apis/engineApi';
 import ToolbarHandlersApi from './apis/toolbarHandlersApi';
+import { cherryConfig } from './untils/cherry.config';
 
 function App() {
   const cherry = useRef<Cherry | null>(null)
   useLayoutEffect(() => {
     if (!cherry.current) {
-      cherry.current = new Cherry({
-        id: "cherry-markdown",
-        value: "# Hello World",
-        locale: "en_US",
-      });
+      cherry.current = new Cherry(
+        cherryConfig,
+      );
     }
   }, []);
 
@@ -22,8 +21,8 @@ function App() {
     <div className='home'>
       <div className='apis' >
         <APIs cherry={cherry} />
-        <EngineApi cherry={cherry}/>
-        <ToolbarHandlersApi cherry={cherry}/>
+        <EngineApi cherry={cherry} />
+        <ToolbarHandlersApi cherry={cherry} />
       </div>
       <div id="cherry-markdown" />
     </div>
---
+++ src/untils/cherry.config.ts
@@ -0,0 +1,46 @@
+export const cherryConfig = {
+  id: "cherry-markdown",
+  value: "# Hello World",
+  locale: "en_US",
+  toolbars: {
+    theme: 'dark', // light or dark
+    showToolbar: true, // False: the top toolbar is not displayed; True: display toolbar; toolbars. Showtoolbar = false and toolbars Toolbar = false equivalent
+    toolbar: [
+      'bold',
+      'italic',
+      'strikethrough',
+      '|',
+      'color',
+      'header',
+      '|',
+      'list',
+      'quote',
+      'table',
+      // 'line-table',
+      // 'bar-table',
+      // 'quicktable',
+      {
+        insert: [
+          'image',
+          'audio',
+          'video',
+          'link',
+          'hr',
+          'br',
+          'code',
+          'formula',
+          'toc',
+          'pdf',
+          'word',
+          'panel',
+
+        ],
+      },
+      'graph',
+      'settings',
+    ],
+    sidebar: [],
+    bubble: ['bold', 'italic', 'underline', 'strikethrough', 'sub', 'sup', 'quote', '|', 'size', 'color'], // array or false
+    float: ['h1', 'h2', 'h3', '|', 'checklist', 'quote', 'quickTable', 'code'], // array or false
+  },
+}
